d00655833b026ffa9fee6ad2a31a6a6e0c0df327
Jaecen <jason@addington.com>
2015-10-03 17:25:23 -0700

    Refactor deckTree.js to use common builder classes for serializatio and deserialization.

--- src/deckTree.js
+++ src/deckTree.js
@@ -3,19 +3,21 @@ const iterUtil = require('./iterUtil');
 
 const BoardType = 'board'; 
 const CardType = 'card'; 
-const DeckType = 'deck'; 
+const DecklistType = 'decklist'; 
 
-function buildObject(type, entity, buildHandlers) {
-	if(!buildHandlers.has(type)) {
+// ==== Serialization/deserialization
+
+function serializeToObject(builders, type, entity) {
+	if(!builders.has(type)) {
 		return null;
 	}
 
-	const { childrenSelector, objectBuilder } = buildHandlers.get(type);
+	const { childrenSelector, objectBuilder } = builders.get(type);
 	const children = childrenSelector(entity)
 		.sort(({ sortKey: left }, { sortKey: right }) => 
 			left.localeCompare(right))
 		.map(({ type: childType, sortKey: childSortKey, entity: childEntity }) => 
-			buildObject(childType, childEntity, buildHandlers));
+			serializeToObject(builders, childType, childEntity));
 	
 	return {
 		entity: entity,
@@ -24,51 +26,87 @@ function buildObject(type, entity, buildHandlers) {
 	};
 }
 
-const buildHandlers = new Map([
-	[
-		DeckType,
-		{
-			'childrenSelector': entity => entity.boards
-				.map(board => ({
-					'type': BoardType,
-					'sortKey': normalizeName(board.type), 
-					'entity': board,
-				})),
-			
-			'objectBuilder': (entity, children) => ({
-				'hash': buildHash(children.map(child => child.object.hash)),
-				'boards': children.map(child => ({ type: normalizeName(child.entity.type), hash: child.object.hash })),
-			}),
-		}
-	],
-	[
-		BoardType,
-		{
-			'childrenSelector': entity => entity.cards
-				.map(card => ({
-					'type': CardType, 
-					'sortKey': normalizeName(card.name), 
-					'entity': card
-				})),
-			
-			'objectBuilder': (entity, children) => ({
-				'hash': buildHash(children.map(child => `${ child.entity.quantity } ${ child.object.hash }`)),
-				'cards': children.map(child => ({ quantity: child.entity.quantity, hash: child.object.hash })),
-			}),
-		} 
-	],
-	[
-		CardType,
-		{
-			'childrenSelector': entity => [],
-			
-			'objectBuilder': (entity, children) => ({
-				'hash': buildHash(normalizeName(entity.name)),
-				'normalizedName': normalizeName(entity.name),
-			}),
-		}
-	]
+function deserializeToEntity(builders, objectRepository, type, hash, reference) {
+	if(!builders.has(type)) {
+		Promise.resolve(null);
+	}
+
+	const { entityBuilder } = builders.get(type); 
+	return objectRepository
+		.get(hash)
+		.then(object => entityBuilder(
+			object,
+			reference,
+			(type, hashes) => Promise.all(
+				hashes.map(({ hash, reference }) => 
+					deserializeToEntity(builders, objectRepository, type, hash, reference)))))
+}
+
+// ==== Builders
+
+const decklistBuilder = {
+	'childrenSelector': entity => entity.boards
+		.map(board => ({
+			'type': BoardType,
+			'sortKey': normalizeName(board.type), 
+			'entity': board,
+		})),
+	
+	'objectBuilder': (entity, children) => ({
+			'hash': buildHash(children.map(child => child.object.hash)),
+			'boards': children.map(child => ({ type: normalizeName(child.entity.type), hash: child.object.hash })),
+		}),
+		
+	'entityBuilder': (object, reference, getEntities) => 
+		getEntities(BoardType, object.boards.map(board => ({ hash: board.hash, reference: board })))
+		.then(boards => ({
+			boards: boards,
+		})) 
+};
+
+const boardBuilder = {
+	'childrenSelector': entity => entity.cards
+		.map(card => ({
+			'type': CardType, 
+			'sortKey': normalizeName(card.name), 
+			'entity': card
+		})),
+	
+	'objectBuilder': (entity, children) => ({
+			'hash': buildHash(children.map(child => `${ child.entity.quantity } ${ child.object.hash }`)),
+			'cards': children.map(child => ({ quantity: child.entity.quantity, hash: child.object.hash })),
+		}),
+	
+	'entityBuilder': (object, reference, getEntities) => 
+		getEntities(CardType, object.cards.map(card => ({ hash: card.hash, reference: card })))
+		.then(cards => ({
+			type: reference.type,
+			cards: cards,
+		}))
+
+};
+
+const cardBuilder = {
+	'childrenSelector': entity => [],
+	
+	'objectBuilder': (entity, children) => ({
+			'hash': buildHash(normalizeName(entity.name)),
+			'normalizedName': normalizeName(entity.name),
+		}),
+	
+	'entityBuilder': (object, reference, getEntities) => ({
+			quantity: reference.quantity, 
+			name: object.normalizedName
+		})
+}; 
+
+const builders = new Map([
+	[ DecklistType, decklistBuilder ],
+	[ BoardType, boardBuilder ],
+	[ CardType, cardBuilder ]
 ]);
+
+// ==== Utility functions
 
 function buildHash(values) {
 	const sum = crypto.createHash('sha256')
@@ -94,54 +132,12 @@ function* iterateTreeObjects(node) {
 	yield node.object;
 }
 
-//=================================================================
-
-function walkObject(objectBuilder, objectRepository, type, hash, reference) {
-	return objectBuilder.has(type)
-		? objectRepository
-			.get(hash)
-			.then(object => objectBuilder.get(type)(
-				object,
-				reference,
-				(type, hashes) => Promise.all(
-					hashes.map(({ hash, reference }) => 
-						walkObject(objectBuilder, objectRepository, type, hash, reference)))
-			))
-		: Promise.resolve(null);
-}
-
-const objectBuilder = new Map([
-	[
-		DeckType, 
-		(object, reference, getEntities) => 
-			getEntities(BoardType, object.boards.map(board => ({ hash: board.hash, reference: board })))
-			.then(boards => ({
-				boards: boards,
-			}))
-	],
-	[
-		BoardType,
-		(object, reference, getEntities) => 
-			getEntities(CardType, object.cards.map(card => ({ hash: card.hash, reference: card })))
-			.then(cards => ({
-				type: reference.type,
-				cards: cards,
-			}))
-	],
-	[
-		CardType,
-		(object, reference, getEntities) => 
- 			({
-				 quantity: reference.quantity, 
-				 name: object.normalizedName
-			}) 
-	],
-]);
+// ==== Exports
 
 module.exports.deserialize = (objectRepository, hash) => 
-	walkObject(objectBuilder, objectRepository, DeckType, hash, null);
+	deserializeToEntity(builders, objectRepository, DecklistType, hash, null);
 	
 module.exports.serialize = function*(deck) {
-	const deckNode = buildObject(DeckType, deck, buildHandlers);
+	const deckNode = serializeToObject(builders, DecklistType, deck);
 	yield* iterateTreeObjects(deckNode);
 }
